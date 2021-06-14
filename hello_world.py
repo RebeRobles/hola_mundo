86ee25d1a8185b4d70a6ce12d85dc9dcb11dfda7
RebeRobles <rebeca.robles@gmail.com>
2021-06-14 08:16:39 -0400

    Actividad de Hola Mundo

---
+++ hello_world.py
@@ -0,0 +1,19 @@
+# 1. TAREA: imprimir "Hola mundo"
+print("Hola mundo")
+# 2a. Almacena tu nombre en una variable y luego úsalo para imprimir la cadena "¡Hola {{tu nombre}}!" usando una coma en la función de impresión
+name = "Rebecca"
+print ("Hola, mi nombre es", name)
+# 2b. Almacena tu nombre en una variable y luego úsalo para imprimir la cadena "¡Hola {{tu nombre}}!" usando un + en la función de impresión
+print (f"Hola, mi nombre es {name}")
+# 3a. Almacena tu número favorito en una variable, y luego úsalo para imprimir la cadena "¡Hola {{num}}!" usando una coma en la función de impresión
+num = 42
+print("Hola", num)
+# 3b. Almacena tu número favorito en una variable, y luego úsalo para imprimir la cadena "¡Hola {{num}}!" usando un + en la función de impresión NINJA BONUS: descubre cómo resolver el error desde arriba, sin cambiar el signo + a una coma
+print(f"Hola {num}")
+# 4a. Almacena 2 de tus comidas favoritas en variables y luego úsalas para imprimir la cadena "Me encanta comer {{food_one}} y {{food_two}}". con el método de formato (# 4a)
+food1 = "sushi"
+food2 = "pizza"
+print("me encanta comer", food1, "y", food2)
+#4b. Almacena 2 de tus comidas favoritas en variables y luego úsalas para imprimir la cadena "Me encanta comer {{food_one}} y {{food_two}}"
+print("Me encanta comer "+ food1 +" y " + food2)
+
